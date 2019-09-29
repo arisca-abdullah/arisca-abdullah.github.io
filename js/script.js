0622fbaa5cc3742c9cdf4218ec6c44b4d384f0a6
Aris <arisca.abdullah08@gmail.com>
2019-09-29 18:33:45 +0700

    Update the site

--- js/script.js
+++ js/script.js
@@ -1,30 +1,36 @@
-let xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 
-let favicon = document.querySelector('.favicon');
-let github_link = document.querySelector('#github-link');
-let avatar = document.querySelector('.jumbotron img');
-let name = document.querySelector('.jumbotron h1');
-let bio = document.querySelector('.jumbotron p');
-let footer = document.querySelector('footer p');
+const favicon = document.querySelector('.favicon');
+const navbar = document.querySelector("nav.navbar");
+const jumbotron = document.querySelector(".jumbotron");
+const footer = document.querySelector("footer");
 
-footer.innerHTML = `Copyright &copy; ${new Date().getFullYear()}`;
+const onresize = () => {
+    jumbotron.style.marginTop = navbar.clientHeight + "px";
+    jumbotron.style.minHeight = (document.documentElement.clientHeight - navbar.clientHeight - footer.clientHeight) + "px";
+}
+
+onresize();
+window.addEventListener("resize", onresize);
 
 xhr.onreadystatechange = function() {
     if(xhr.readyState == 4 && xhr.status == 200) {
-        let res = JSON.parse(this.responseText);
+        const res = JSON.parse(this.responseText);
 
-        avatar.classList.remove('animate1');
-        name.classList.remove('animate2', 'name');
-        bio.classList.remove('animate3', 'bio');
+        jumbotron.querySelector("img").classList.remove('animate1');
+        jumbotron.querySelector("h1").classList.remove('animate2', 'name');
+        jumbotron.querySelector("p").classList.remove('animate3', 'bio');
         
-        avatar.classList.add('img-thumbnail')
+        jumbotron.querySelector("img").classList.add('img-thumbnail')
 
         favicon.setAttribute('href', res.avatar_url);
-        github_link.setAttribute('href', res.html_url)
-        avatar.setAttribute('src', res.avatar_url);
+        document.getElementById("github-link").setAttribute('href', res.html_url);
+        document.getElementById("repos-link").setAttribute('href', res.html_url + "?tab=repositories");
+        jumbotron.querySelector("img").setAttribute('src', res.avatar_url);
+        jumbotron.querySelector(".location").innerHTML = `<i class="fas fa-map-marker-alt"></i><span class="ml-1 mr-2">${res.location}</span>|<a href="${res.html_url + '?tab=followers'}" class="ml-2">${res.followers} Followers</a>`;
 
-        name.innerHTML = res.name;
-        bio.innerHTML = res.bio;
+        jumbotron.querySelector("h1").innerHTML = res.name;
+        jumbotron.querySelector("p").innerHTML = res.bio;
     }
 }
 xhr.open('GET', 'https://api.github.com/users/arisca-abdullah', true);
