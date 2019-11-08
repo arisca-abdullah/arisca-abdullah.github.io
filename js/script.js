c4b58c71ff655b080d695ecf8021289d24c355ec
Aris <arisca.abdullah08@gmail.com>
2019-11-08 21:25:18 +0700

    Update the site

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-const favicon = document.querySelector(".favicon");
 const navbar = document.querySelector("nav.navbar");
 const jumbotron = document.querySelector(".jumbotron");
 const footer = document.querySelector("footer");
@@ -20,11 +19,11 @@ fetch("https://api.github.com/users/arisca-abdullah")
   .then(data => {
     jumbotron.querySelector("img").classList.remove("animate1");
     jumbotron.querySelector("h1").classList.remove("animate2", "name");
-    jumbotron.querySelector("p").classList.remove("animate3", "bio");
+    jumbotron.querySelector(".location").classList.remove("animate2-5", "loading-location");
+    jumbotron.querySelector(".bio").classList.remove("animate3", "loading-bio");
+    jumbotron.querySelector(".extra").classList.remove("animate3-5", "loading-extra");
 
     jumbotron.querySelector("img").classList.add("img-thumbnail");
-
-    favicon.setAttribute("href", data.avatar_url);
 
     document.getElementById("github-link").setAttribute("href", data.html_url);
     document
@@ -33,13 +32,19 @@ fetch("https://api.github.com/users/arisca-abdullah")
 
     jumbotron.querySelector("img").setAttribute("src", data.avatar_url);
     jumbotron.querySelector(".location").innerHTML = `
-    <i class="fas fa-map-marker-alt"></i>
-    <span class="ml-1 mr-2">${data.location}</span>|
-    <a href="${data.html_url + "?tab=followers"}" class="ml-2">
+    <i class="fas fa-map-marker-alt mr-1"></i> Live in ${data.location}
+    `;
+    jumbotron.querySelector(".extra").innerHTML = `
+    <a class="mx-2" href="${data.html_url + "?tab=repositories"}">
+        <i class="fas fa-folder-open"></i>
+        ${data.public_repos} Repositories
+    </a>|
+    <a class="mx-2" href="${data.html_url + "?tab=followers"}">
+        <i class="fas fa-users"></i>
         ${data.followers} Followers
     </a>
     `;
 
     jumbotron.querySelector("h1").innerHTML = data.name;
-    jumbotron.querySelector("p").innerHTML = data.bio;
+    jumbotron.querySelector(".bio").innerHTML = data.bio;
   });
